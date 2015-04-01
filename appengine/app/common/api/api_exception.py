85bdbac945b4a5764d6c6c76f4a2769a562b73e6
Antonio Zugaldia <antonio@zugaldia.net>
2015-04-01 14:57:41 -0400

    migration to appython started

--- appengine/app/common/api/api_exception.py
+++
@@ -1,8 +0,0 @@
-'''
-The API will always use this exception for errors. These are messages sent to
-the developer and are *not* tracked by Bugsnag.
-'''
-
-
-class ApiException(Exception):
-    pass
--- appengine/app/module_default/default/views/web/landing.py
+++
@@ -1,9 +0,0 @@
-from flask import redirect
-from flask.views import MethodView
-
-import logging
-
-
-class WebLandingView(MethodView):
-    def get(self):
-        return redirect('https://github.com/zugaldia/capitalbikeshare')
